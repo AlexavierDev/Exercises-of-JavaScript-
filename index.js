0e1997f921b0c35a82715cd32f5064aa0328d96f
alexavier <alexavier859@gmail.com>
2020-07-20 11:58:58 -0300

    Crie um programa para realizar operações bancárias na conta de um usuário.  Comece criando um objeto com o nome do usuário, suas transações e saldo.  As transações (transactions) devem iniciar como um array vazio `[]` e o saldo (balance) em `0` (zero).  ```js const user = {   name: "Mariana",   transactions: [],   balance: 0 }; ```  ### Adicionar transações  Crie uma função `createTransaction` para adicionar uma nova transação no array de transações de um usuário, essa função deve receber como parâmetro um objeto de transação que tem o seguinte formato:  ```js {   type: 'credit',   value: 50.5 } ```  O `type` pode ser `credit` para créditos e `debit` para débitos da conta do usuário.  Quanto uma transação for do tipo `credit` ela deve também somar o valor do crédito no saldo (balance) do usuário.  Se for uma transação do tipo `debit` ela deve subtrair o valor do débito no saldo (balance) do usuário.  _Dica.: Você pode usar o método `user.transactions.push(transaction)` para adicionar um novo item no array de transações.

---
+++ index.js
@@ -0,0 +1,16 @@
+const user = {
+    name: 'Alex',
+    transations: [],
+    balance: 0
+}
+
+function createTransaction(transaction) {
+    user.transactions.push(transaction)
+
+    if (transaction.type === 'credit') {
+        user.balance = user.balance + transaction.value
+    } else {
+        user.balance = user.balance - transaction.value
+    }
+}
+
